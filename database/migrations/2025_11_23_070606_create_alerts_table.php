00abb06297ea53e30a298bac72d698a66f1be9e2
sm4640 <s1m0611@nvaer.com>
2025-11-23 19:42:07 +0900

    laravel & pgsql setting

---
+++ database/migrations/2025_11_23_070606_create_alerts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->bigIncrements('id');      // 순차 증가 id
+            $table->string('name');           // 알람 이름
+            $table->string('severity');       // 심각도
+            $table->string('instance');       // 발생 인스턴스
+            $table->text('summary');          // 요약/상세
+
+            $table->timestampTz('create_at')->useCurrent();
+            $table->timestampTz('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->string('status')->default('unseen'); 
+            // unseen / in_progress / done
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alerts');
+    }
+};
