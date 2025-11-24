ef0dca704a778e579e4ff3c8789fb5aedf8de453
sm4640 <s1m0611@nvaer.com>
2025-11-24 19:53:32 +0900

    feat: 알람 목록 controller 구현

---
+++ app/Http/Controllers/AlertController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alert;
+use App\Models\AlertLog;
+use Illuminate\Http\Request;
+
+class AlertController extends Controller
+{
+    public function index()
+    {
+        $unseen = Alert::where('status', 'unseen')
+                       ->orderByDesc('created_at')
+                       ->get();
+
+        $inProgress = Alert::where('status', 'in_progress')
+                           ->orderByDesc('created_at')
+                           ->get();
+
+        $done = Alert::where('status', 'done')
+                     ->orderByDesc('created_at')
+                     ->get();
+
+        return view('alerts.index', compact('unseen', 'inProgress', 'done'));
+    }
+}
